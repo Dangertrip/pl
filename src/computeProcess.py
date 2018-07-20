78fbc449d1c6a711303b44bb33dd25c46abb2889
dangertrip <dangertrip@hotmail.com>
2018-07-19 23:34:13 -0500

    pl

--- src/computeProcess.py
+++ src/computeProcess.py
@@ -4,12 +4,30 @@ from trim import Trim
 import bsplot
 
 def computeProcess(param):
-    ProcessObject=[Fastqc(),Trim(),Bsmap()]
+    fastqc,trim,bsmap = Fastqc(), Trim(), Bsmap()
+    ProcessObject=[fastqc,trim,bsmap]
     for obj in ProcessObject:
         status,word=obj.check()
         if not status:
             raise Exception(word)
     #check all software have been installed successfully.
+    for obj in ProcessObject:
+        obj.setpath('./')
     
+    name = param['name']
+    resultfilename={}
     if param['qc']:
+        fn=[]
+        for nn in name:
+            temp=[]
+            for n in nn:
+                temp.append(fastqc.run(n))
+            fn.append(temp)
+        resultfilename['qc']=fn
+    if param['trim']:
+        fn=[]
+        
+         
+        
+
         
